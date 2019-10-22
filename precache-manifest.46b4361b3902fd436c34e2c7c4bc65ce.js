fa4f22fde139c4938f900c095af7a3dddfb0a052
Jose Angel Roa Gomez <jangelroa@gmail.com>
2019-10-21 23:22:48 -0700

    Updates

---
+++ precache-manifest.46b4361b3902fd436c34e2c7c4bc65ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c489454bb42b05d77982c92a43255d79",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b4faab86988af1445570",
+    "url": "/monsters-rolodex/static/css/main.04b0c1ae.chunk.css"
+  },
+  {
+    "revision": "b42ad286debf87e51faf",
+    "url": "/monsters-rolodex/static/js/2.14105919.chunk.js"
+  },
+  {
+    "revision": "b4faab86988af1445570",
+    "url": "/monsters-rolodex/static/js/main.e5f1a115.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
